67c72280cf5f6131ef9445c19bb94364ed166edd
Andrei Betlen <abetlen@gmail.com>
2023-04-10 03:34:43 -0400

    Add project scaffolding files

---
+++ setup.py
@@ -0,0 +1,27 @@
+from skbuild import setup
+
+setup(
+    name="rwkv_cpp_python",
+    description="A Python wrapper for rwkv.cpp",
+    long_description_content_type="text/markdown",
+    version="0.0.1",
+    author="",
+    author_email="",
+    license="MIT",
+    package_dir={"rwkv_cpp": "rwkv"},
+    packages=["rwkv_cpp"],
+    install_requires=[
+        "numpy>=1.24.1",
+        "torch>=2.0.0",
+        "tokenizers>=0.13.3",
+    ],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
