a155f92bccf78635b7c8bb07fb2866e3d98dfd3b
Locria Cyber <74560659+iacore@users.noreply.github.com>
2023-04-14 17:08:12 +0000

    Add dump.cpp to show model info

--- dump.cpp
+++ dump.cpp
@@ -6,12 +6,12 @@ void print_shape() {
 
 }
 
-int main() {
-    auto ctx = rwkv_init_from_file("RWKV-4-Pile-169M-20220807-8023-q4_1.bin", 1);
+int main(int argc, char* argv[]) {
+    if (argc < 2) return 1;
+    auto ctx = rwkv_init_from_file(argv[1], 1);
     // print_shape("token_index", ctx->token_index);
     // print_shape("state", ctx->state);
     // print_shape("logits", ctx->logits);
     // print_shape(ctx->state_parts[ctx->model->n_layer * 5]
-
     // ggml_graph_dump_dot(ctx->graph, NULL, "graph.dot");
 }
