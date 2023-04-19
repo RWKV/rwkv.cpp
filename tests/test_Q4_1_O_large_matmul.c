d9d7467e3bd6555d24cab5c6773571060a2395d1
saharNooby <saharnooby@protonmail.com>
2023-04-19 16:30:35 +0400

    Remove debug logging

--- tests/test_Q4_1_O_large_matmul.c
+++ tests/test_Q4_1_O_large_matmul.c
@@ -36,70 +36,38 @@ int main(int argc, const char ** argv) {
         .no_alloc   = false,
     };
 
-    fprintf(stderr, "%d\n", __LINE__);
-
     struct ggml_context * ctx = ggml_init(params);
 
-    fprintf(stderr, "%d\n", __LINE__);
-
     struct ggml_tensor * mat = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, MATRIX_SIZE, MATRIX_SIZE);
-
-    fprintf(stderr, "%d\n", __LINE__);
 
     for (int i = 0; i < MATRIX_SIZE * MATRIX_SIZE; i++) {
         GGML_SET_ELEMENT_F32(mat, i, RANDOM_FLOAT());
     }
-
-    fprintf(stderr, "%d\n", __LINE__);
 
     // Add some outliers
     for (int i = 0; i < MATRIX_SIZE; i++) {
         GGML_SET_ELEMENT_F32(mat, i * MATRIX_SIZE + 1, RANDOM_FLOAT() * 100.0F);
     }
 
-    fprintf(stderr, "%d\n", __LINE__);
-
     struct ggml_tensor * quantized_mat = ggml_new_tensor_2d(ctx, GGML_TYPE_Q4_1_O, MATRIX_SIZE, MATRIX_SIZE);
-
-    fprintf(stderr, "%d\n", __LINE__);
 
     int64_t histogram[16];
 
-    fprintf(stderr, "%d\n", __LINE__);
-
     ggml_quantize_q4_1_o(mat->data, quantized_mat->data, MATRIX_SIZE * MATRIX_SIZE, QK, histogram);
 
-    fprintf(stderr, "%d\n", __LINE__);
-
     struct ggml_tensor * vec = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, MATRIX_SIZE);
-
-    fprintf(stderr, "%d\n", __LINE__);
 
     for (int i = 0; i < MATRIX_SIZE; i++) {
         GGML_SET_ELEMENT_F32(vec, i, RANDOM_FLOAT());
     }
 
-    fprintf(stderr, "%d\n", __LINE__);
-
     struct ggml_tensor * expected_result = ggml_mul_mat(ctx, mat, vec);
-
-    fprintf(stderr, "%d\n", __LINE__);
     struct ggml_tensor * quantized_result = ggml_mul_mat(ctx, quantized_mat, vec);
 
-    fprintf(stderr, "%d\n", __LINE__);
-
     struct ggml_cgraph graph = ggml_build_forward(expected_result);
-
-    fprintf(stderr, "%d\n", __LINE__);
     ggml_build_forward_expand(&graph, quantized_result);
-
-    fprintf(stderr, "%d\n", __LINE__);
     graph.n_threads = 4;
-
-    fprintf(stderr, "%d\n", __LINE__);
     ggml_graph_compute(ctx, &graph);
-
-    fprintf(stderr, "%d\n", __LINE__);
 
     float diff_sum = 0.0F;
 
@@ -107,20 +75,12 @@ int main(int argc, const char ** argv) {
         diff_sum += fabsf(GGML_GET_ELEMENT_F32(expected_result, i) - GGML_GET_ELEMENT_F32(quantized_result, i));
     }
 
-    fprintf(stderr, "%d\n", __LINE__);
-
     float diff_average = diff_sum / MATRIX_SIZE;
-
-    fprintf(stderr, "%d\n", __LINE__);
 
     // More strict test is in test_Q4_1_O.c, here we just do sanity check
     GGML_ASSERT(diff_average <= 2.0F, "Unexpected average difference value %f", diff_average);
 
-    fprintf(stderr, "%d\n", __LINE__);
-
     ggml_free(ctx);
-
-    fprintf(stderr, "%d\n", __LINE__);
 
     return 0;
 }
