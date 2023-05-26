5b1237f1a299a26fdf052568be39ad7099dd0e04
saharNooby <saharnooby@protonmail.com>
2023-05-26 19:22:21 +0400

    Use types from typing for better compatibility with older Python versions

--- rwkv/chat_with_bot.py
+++ rwkv/chat_with_bot.py
@@ -12,7 +12,7 @@ import tokenizers
 import rwkv_cpp_model
 import rwkv_cpp_shared_library
 import json
-from typing import Optional
+from typing import List, Dict, Optional
 
 # ======================================== Script settings ========================================
 
@@ -66,11 +66,11 @@ prompt_token_count = len(prompt_tokens)
 
 # =================================================================================================
 
-processed_tokens: list[int] = []
+processed_tokens: List[int] = []
 logits: Optional[torch.Tensor] = None
 state: Optional[torch.Tensor] = None
 
-def process_tokens(_tokens: list[int], new_line_logit_bias: float = 0.0) -> None:
+def process_tokens(_tokens: List[int], new_line_logit_bias: float = 0.0) -> None:
     global processed_tokens, logits, state
 
     processed_tokens += _tokens
@@ -80,7 +80,7 @@ def process_tokens(_tokens: list[int], new_line_logit_bias: float = 0.0) -> None
 
     logits[END_OF_LINE_TOKEN] += new_line_logit_bias
 
-state_by_thread: dict[str, dict] = {}
+state_by_thread: Dict[str, Dict] = {}
 
 def save_thread_state(_thread: str) -> None:
     state_by_thread[_thread] = {
@@ -226,8 +226,8 @@ Below is an instruction that describes a task. Write a response that appropriate
         print(f'> {bot}{separator}', end='')
 
     start_index: int = len(processed_tokens)
-    accumulated_tokens: list[int] = []
-    token_counts: dict[int, int] = {}
+    accumulated_tokens: List[int] = []
+    token_counts: Dict[int, int] = {}
 
     for i in range(MAX_GENERATION_LENGTH):
         for n in token_counts:
