10d973aa7a9b0c8b70134460774be0cf3013264d
Molly Sophia <mollysophia379@gmail.com>
2024-06-22 12:08:03 +0800

    Fix sequence mode for RWKV v6
    Thanks to @cryscan
    
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- rwkv.cpp
+++ rwkv.cpp
@@ -49,6 +49,8 @@ static_assert(sizeof(decltype(ftell(NULL))) >= 8, "File offsets should be 64-bit
 
 #include "rwkv_operators_wkv_v5.inc"
 
+#include "rwkv_operators_wkv_v6.inc"
+
 #include "rwkv_graph.inc"
 
 // API function.
